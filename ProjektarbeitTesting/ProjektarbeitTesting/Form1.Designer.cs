58594e89f1cf82525bc2b07c8e13498f00682f34
Mark080503 <142298739+Mark080503@users.noreply.github.com>
2023-08-15 11:02:28 +0200

    Button ändert label1 Text und Dimensionen

--- ProjektarbeitTesting/ProjektarbeitTesting/Form1.Designer.cs
+++ ProjektarbeitTesting/ProjektarbeitTesting/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(356, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProjektarbeitTesting/ProjektarbeitTesting/Form1.cs
+++ ProjektarbeitTesting/ProjektarbeitTesting/Form1.cs
@@ -16,5 +16,25 @@ namespace ProjektarbeitTesting
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+            // Define the relative increase values for font size and dimensions
+            int fontSizeIncrease = 2; // points
+            int dimensionsIncrease = 20; // pixels
+
+            // Increase font size by the defined amount
+            float newFontSize = Math.Max(label1.Font.Size + fontSizeIncrease, 6); // Minimum font size of 6 points
+            label1.Font = new Font(label1.Font.FontFamily, newFontSize);
+
+            // Increase label dimensions by the defined amount
+            label1.Width += dimensionsIncrease;
+            label1.Height += dimensionsIncrease;
+        }
     }
 }
