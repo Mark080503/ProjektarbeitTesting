fcbbf25ac31f5da03343ece8b5bf55683f3c8925
Mark080503 <142298739+Mark080503@users.noreply.github.com>
2023-08-15 15:34:49 +0200

    Update

--- Kalkulator/Kalkulator/Form1.Designer.cs
+++ Kalkulator/Kalkulator/Form1.Designer.cs
@@ -286,7 +286,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 525);
+            this.ClientSize = new System.Drawing.Size(625, 571);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonEquals);
             this.Controls.Add(this.buttonSubtraction);
